01de7ad2004bf4e38c5106359d0d1e0f1da7454b
fkus <fkus@local>
2017-05-13 13:49:01 +0200

    test

--- public/assets/js/plugins/promo.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-  $(".fullscreen-static-image").backstretch(["assets/img-temp/promo/promo1.png", "assets/img-temp/promo/promo2.png"],
-  {duration: 5000, fade:1500});
-});
--- web/metronic/html.page.breadcrumb.php
+++
@@ -1,9 +0,0 @@
-<ul class="page-breadcrumb breadcrumb">
-    <li>
-        <a href="/">Home</a>
-        <i class="fa fa-circle"></i>
-    </li>
-    <li>
-        <span class="active">Customer</span>
-    </li>
-</ul><?php
